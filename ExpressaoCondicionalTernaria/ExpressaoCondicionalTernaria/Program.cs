f44f3aff68896be1bd438d135ac61c313ce52294
virgiliobreda <virgiliobred031@gmail.com>
2023-11-06 13:18:00 -0300

    commit

---
+++ ExpressaoCondicionalTernaria/ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace ExpressaoCondicionalTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
