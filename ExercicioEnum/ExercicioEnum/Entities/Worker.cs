8d72f5e6ec37d7690118016e0a37df58aea8fab5
virgiliobreda <virgiliobred031@gmail.com>
2023-11-10 14:04:38 -0300

    commit

---
+++ ExercicioEnum/ExercicioEnum/Entities/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioEnum.Entities.Enums;
+
+namespace ExercicioEnum.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+
+        public Worker() { }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract) 
+        { 
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public Double Income(int year, int mouth)
+        {
+            double sum = BaseSalary;
+            
+            foreach(HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == mouth)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
+        }
+    }
+}
