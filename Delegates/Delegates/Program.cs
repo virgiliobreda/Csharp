85afe4aada66b5ef270e67f81f72936155ea39ba
virgiliobreda <virgiliobred031@gmail.com>
2024-01-17 09:34:22 -0300

    commit

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using Delegates.Services;
+
+namespace Delegates
+{ delegate double BinaryNumericOperetion(double n1, double n2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperetion op = CalculationService.Max;
+
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+
+            
+        }
+    }
+}
---
+++ MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using MulticastDelegates.Services;
+
+namespace MulticastDelegates
+{
+    internal class Program
+    {
+        delegate void BinaryNumberOperation(double n1,  double n2);
+        static void Main(string[] args)
+        {
+            BinaryNumberOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            
+
+            double a = 10;
+            double b = 12;
+
+            op.Invoke(a, b);
+        }
+    }
+}
