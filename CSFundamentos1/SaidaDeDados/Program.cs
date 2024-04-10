8eeca6fdcf6244a14be62b19f50f7b9148217eb2
virgiliobreda <virgiliobred031@gmail.com>
2024-04-10 17:32:03 -0300

    commit

---
+++ CSFundamentos1/SaidaDeDados/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Saída de dados : Formatação");
+Console.WriteLine();
+
+int idade = 25;
+string nome = "Maria";
+Console.Write(nome);
+Console.WriteLine(idade);
+
+//Escreve na mesma linha 'maria tem 25 anos'
+Console.Write(nome);
+Console.Write(" tem ");
+Console.Write(idade);
+Console.Write(" anos \n");
+
+
+//usar a concatenação : usando o operador +
+Console.WriteLine(nome + " tem " + idade + " anos ");
+
+//usar a interpolação de string : $ -> a interpolação {}
+Console.WriteLine($"{nome} tem {idade} anos");
+
+//usar place holders : usa {} com numeração com inicio em zero
+Console.WriteLine("{0} tem {1} anos", nome, idade);
+
+Console.ReadLine();
