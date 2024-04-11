0f9a03b2d06c653e5fef73f7bde0994af8d77376
virgiliobreda <virgiliobred031@gmail.com>
2024-04-11 12:00:19 -0300

    commit

---
+++ CSFundamentos1/Constantes/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("## Constantes ##\n");
+
+//calculo da area e perimetro do circulo
+double area, perimetro, raio;
+//const double PI = 3.14;
+
+Console.WriteLine("Informe o raio do círculo");
+raio = Convert.ToDouble(Console.ReadLine());
+
+perimetro = 2 * Math.PI * raio;
+area = Math.PI * Math.Pow(raio,2);
+
+Console.WriteLine($"Perímetro = {perimetro}");
+Console.WriteLine($"Área = {area}");
+
+Console.WriteLine();
---
+++ CSFundamentos1/OperadoresAtribuição/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("## Operadores de atribuição ##\n");
+
+//var x = 10;
+//Console.WriteLine($"Valor inicial de x = {x}");
+//Console.WriteLine($"x += 5 -> {x += 5}");
+//Console.WriteLine($"x -= 5 -> {x -= 3}");
+//Console.WriteLine($"x *= 5 -> {x *= 4}");
+//Console.WriteLine($"x /= 5 -> {x /= 5}");
+//Console.WriteLine($"x %= 5 -> {x %= 5}");
+
+var y = "123";
+Console.WriteLine($"Valor inicial de y é igual a {y}");
+y += "456";
+Console.WriteLine($"y += \"456\" -> {y}");
+
+Console.ReadKey();
